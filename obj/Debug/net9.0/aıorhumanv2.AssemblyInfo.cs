7400f86e62d51ab601cccec63a64b7b0c7c1912d
EnessTalay <enesttalay@gmail.com>
2025-12-26 12:20:06 +0300

    Test Ve Dökümantasyon

--- obj/Debug/net9.0/aıorhumanv2.AssemblyInfo.cs
+++ obj/Debug/net9.0/aıorhumanv2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("aıorhumanv2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44e9109d1398265fbd8625bb0acfcb54cc7b7404")]
 [assembly: System.Reflection.AssemblyProductAttribute("aıorhumanv2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("aıorhumanv2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
